79595a1cfd53551f65230b8b9f46d6cb24fa5d5b
priyankraychura <priyankraychura@gmail.com>
2024-07-12 21:03:43 +0530

    udf used

--- 14. revName.py
+++ 14. revName.py
@@ -1,7 +1,10 @@
+def reverseName(name):
+    revName = ""
+
+    for i in range(len(name)-1, 0-1, -1):
+        revName += name[i]
+
+    return revName
+
 name = input("Enter you name: ")
-revName = ""
-
-for i in range(len(name)-1, 0-1, -1):
-    revName += name[i]
-
-print("Reversed of enter name is: ",revName)
+print("Reversed of enter name is: ",reverseName(name))
