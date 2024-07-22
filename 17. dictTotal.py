7dc3c784d04435a7640445ccb938a5e34fa6236d
priyankraychura <priyankraychura@gmail.com>
2024-07-22 16:01:19 +0530

    dictionary total programme added

---
+++ 17. dictTotal.py
@@ -0,0 +1,13 @@
+marks = {
+    'python': 98,
+    'j2ee': 85,
+    'cs': 70,
+    'practicle': 100
+}
+
+total = 0
+
+for sub in marks:
+    total += marks[sub]
+
+print("Total of students marks: ",total)
